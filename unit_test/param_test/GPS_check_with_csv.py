033a419cf423ad1e47009c633a700dd5e01eade1
hookunsencho <namiharrypotter@yahoo.co.jp>
2023-06-07 21:30:38 +0900

    [Modify]GPS_check_with_csv

--- unit_test/param_test/GPS_check_with_csv.py
+++ unit_test/param_test/GPS_check_with_csv.py
@@ -1,53 +1,32 @@
 import asyncio
 from mavsdk import System
 
-
 async def run():
+    #座標格納用配列
+    latitude_list = []
+    longitude_list = []
+
     # Init the drone
     drone = System()
     await drone.connect(system_address="serial:///dev/ttyACM0:115200")
-    print("Waiting for drone to connect...")
+    #await drone.connect(system_address="udp://:14540")
+    print("waiting for connect...")
     async for state in drone.core.connection_state():
-
-        if state.is_connected:
-            print(f"-- Connected to drone!")
+        if state.is_connected : 
+            print("rpi<->pix : connected!")
             break
-    # Start the task
-    asyncio.ensure_future(create_position_dict(drone))
-
+    
+    #現在地を配列に格納(10要素)
     while True:
-        await asyncio.sleep(1)
-
-async def create_position_dict(drone) -> None:
-        # log_file = self.create_log_file()
-        log_dict = {}
-        log_dict["lat_deg"] = []
-        log_dict["lng_deg"] = []
-        log_dict["abs_alt_m"] = []
-        log_dict["rel_alt_m"] = []
-        # get_position(self)
         async for position in drone.telemetry.position():
-            latitude_deg = position.latitude_deg
-            longitude_deg = position.longitude_deg
-            absolute_altitude_m = position.absolute_altitude_m
-            relative_altitude_m = position.relative_altitude_m
-        log_dict["lat_deg"].append(latitude_deg)
-        log_dict["lng_deg"].append(longitude_deg)
-        log_dict["abs_alt_m"].append(absolute_altitude_m)
-        log_dict["rel_alt_m"].append(relative_altitude_m)
-        print(log_dict)
+            latitude_list.append(position.latitude_deg)
+            longitude_list.append(position.longitude_deg)
+            print(latitude_list)
+            print(longitude_list)
 
-
-# async def get_position(self) -> None:
-#     """get gps position in (lat, lng, abusolute altitude, relative altitude)"""
-#     async for position in self.drone.telemetry.position():
-#         self.latitude_deg = position.latitude_deg
-#         self.longitude_deg = position.longitude_deg
-#         self.absolute_altitude_m = position.absolute_altitude_m
-#         self.relative_altitude_m = position.relative_altitude_m
-
-
+            break #async forのループから抜け出す
 
 if __name__ == "__main__":
-    # Start the main function
-    asyncio.run(run())
+    #run()関数を一度だけ実行する
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run())
