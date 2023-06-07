8098c1acfdc05e65dc8383927370461589ff14a2
Kwskayumu <kwskayumu@icloud.com>
2023-06-07 16:54:45 +0900

    [Modify] lidar test 23

--- unit_test/sensor_test/lidar_test_tfmini23.py
+++ unit_test/sensor_test/lidar_test_tfmini23.py
@@ -9,7 +9,7 @@ def getTFminiData():
         #time.sleep(0.1)
         count = ser.in_waiting
         if count > 8:
-            recv = ser.read(9)  
+            recv = ser.read(9)
             ser.reset_input_buffer()  
             # type(recv), 'str' in python2(recv[0] = 'Y'), 'bytes' in python3(recv[0] = 89)
             # type(recv[0]), 'str' in python2, 'int' in python3 
