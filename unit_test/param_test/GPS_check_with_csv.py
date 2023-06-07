23e1da8de64b48019bfbc531c045f58d6ff429ac
hookunsencho <namiharrypotter@yahoo.co.jp>
2023-06-07 18:02:04 +0900

    [Modify]GPS_check_with_csv

--- unit_test/param_test/GPS_check_with_csv.py
+++ unit_test/param_test/GPS_check_with_csv.py
@@ -8,10 +8,10 @@ async def run():
     await drone.connect(system_address="serial:///dev/ttyACM0:115200")
 
     # Start the task
-    task = cycle_record_log()
+    task = cycle_record_log(drone)
     await task
 
-async def cycle_record_log(self) -> None:
+async def cycle_record_log(drone) -> None:
         # log_file = self.create_log_file()
         log_dict = {}
         log_dict["mode"] = []
@@ -21,15 +21,15 @@ async def cycle_record_log(self) -> None:
         log_dict["rel_alt_m"] = []
         while True:
             # get_position(self)
-            async for position in self.drone.telemetry.position():
-                self.latitude_deg = position.latitude_deg
-                self.longitude_deg = position.longitude_deg
-                self.absolute_altitude_m = position.absolute_altitude_m
-                self.relative_altitude_m = position.relative_altitude_m
-            log_dict["lat_deg"].append(self.latitude_deg)
-            log_dict["lng_deg"].append(self.longitude_deg)
-            log_dict["abs_alt_m"].append(self.absolute_altitude_m)
-            log_dict["rel_alt_m"].append(self.relative_altitude_m)
+            async for position in drone.telemetry.position():
+                latitude_deg = position.latitude_deg
+                longitude_deg = position.longitude_deg
+                absolute_altitude_m = position.absolute_altitude_m
+                relative_altitude_m = position.relative_altitude_m
+            log_dict["lat_deg"].append(latitude_deg)
+            log_dict["lng_deg"].append(longitude_deg)
+            log_dict["abs_alt_m"].append(absolute_altitude_m)
+            log_dict["rel_alt_m"].append(relative_altitude_m)
             print(log_dict)
             await asyncio.sleep(1)
 
