5d5be5bfa2c2334fe0c7b859030d8713fc2e6b33
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-06-07 18:46:56 +0900

    modified

--- unit_test/sensor_test/px4_lidar_test.py
+++ unit_test/sensor_test/px4_lidar_test.py
@@ -3,6 +3,20 @@ from mavsdk import System
 
 async def run():
     drone = System()
+    await drone.connect(system_address="serial:///dev/ttyACM0:115200")
+    
+    print("Waiting for drone to connect...")
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print("Drone discovered!")
+            break
+
+    print("Waiting for drone to have a global position estimate...")
+    async for health in drone.telemetry.health():
+        if health.is_global_position_ok and health.is_home_position_ok:
+            print("-- Global position estimate OK")
+            break
+    
     status_test_task = asyncio .ensure_future(print_status_text(drone))
     async for distance in drone.telemetry.distance_sensor():
         for i in range(2):
