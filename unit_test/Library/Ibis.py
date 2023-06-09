5324ac2bbe48191f0c1b2040aaf20351a13c3628
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-06-09 23:00:12 +0900

    created Ibis modifide arm_test

---
+++ unit_test/Library/Ibis.py
@@ -0,0 +1,23 @@
+import asyncio
+import mavsdk
+import logger
+
+
+class Ibis:
+    
+    def __inti__(self):
+        self.ibis = mavsdk.System()
+    
+    async def connect(self):
+        logger.info("-- Waiting for drone to connect...")
+        await self.connect(system_address="serial:///dev/ttyACM0:115200")
+        async for state in self.ibis.core.connection_state():
+            if state.is_connected:
+                print("Drone connected!")
+                break
+        
+    def arm(self):
+        logger.info("-- Arming")
+        self.ibis.action.arm()
+        logger.info("-- Armed!")
+        
