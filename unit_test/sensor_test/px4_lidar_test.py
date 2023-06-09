d8df396f780cb3d3e8d9f7d9e3bcb1d0055cb2c8
raspichan <cansatibis@gmail.com>
2023-06-09 19:28:07 +0900

    modified

--- unit_test/sensor_test/px4_lidar_test.py
+++ unit_test/sensor_test/px4_lidar_test.py
@@ -2,18 +2,18 @@ import asyncio
 from mavsdk import System
 
 async def run():
-    status_test_task = asyncio .ensure_future(print_status_text(drone))
     drone = System()
+    status_test_task = asyncio .ensure_future(print_status_text(drone))
     async for distance in drone.telemetry.distance_sensor():
         for i in range(2):
              print(f"distance %d: {distance.current_distance_m}"%i)
-    status_text_task.cancel()
+    status_test_task.cancel()
 
 async def print_status_text(drone):
     try:
         async for status_text in drone.telemetry.status_text():
             print(f"Status: {status_text.type}: {status_text.text}")
-    except asyncio.Cancellederror:
+    except asyncio.CancelledError:
         return
 
 if __name__=="__main__":
