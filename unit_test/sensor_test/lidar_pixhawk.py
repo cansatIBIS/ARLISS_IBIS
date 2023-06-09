eae53c531fa68b3f2deb6042a545ac83073c02fe
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-06-09 18:36:22 +0900

    modified

--- unit_test/sensor_test/lidar_pixhawk.py
+++ unit_test/sensor_test/lidar_pixhawk.py
@@ -8,10 +8,11 @@ async def run():
 
     print("Hey")
     drone = System()
+    
+    print("Waiting for drone to connect...")
     await drone.connect(system_address="serial:///dev/ttyACM0:115200")
 
 
-    print("Waiting for drone to connect...")
     async for state in drone.core.connection_state():
         if state.is_connected:
             print(f"-- Connected to drone!")
