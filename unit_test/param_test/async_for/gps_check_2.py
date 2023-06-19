9dfb5249f6e4a7ab883b7c045fa14bd037f371df
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-06-19 16:51:10 +0900

    modified

--- unit_test/param_test/async_for/gps_check_2.py
+++ unit_test/param_test/async_for/gps_check_2.py
@@ -1,9 +1,16 @@
 import asyncio
 from mavsdk import System
 
-drone = System()
 
 async def print_gps():
+    drone = System()
+    await drone.connect(system_address="serial:///dev/ttyACM0:115200")
+    print("Waiting for drone to connect...")
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print("-- Connected to drone!")
+            break
+        
     for i in range(10):
             async for position in drone.telemetry.position():
                 print(position.latitude_deg)
