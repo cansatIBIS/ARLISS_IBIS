1dab75800c36b29e0cefec5364a392c5823fcc09
hookunsencho <namiharrypotter@yahoo.co.jp>
2023-06-19 19:32:55 +0900

    modify

--- unit_test/param_test/GPS_check.py
+++ unit_test/param_test/GPS_check.py
@@ -15,8 +15,8 @@ async def run():
     asyncio.ensure_future(print_in_air(drone))
     asyncio.ensure_future(print_position(drone))
 
-    # while True:
-    #     await asyncio.sleep(1)
+    while True:
+        await asyncio.sleep(1)
 
 
 async def print_battery(drone):
