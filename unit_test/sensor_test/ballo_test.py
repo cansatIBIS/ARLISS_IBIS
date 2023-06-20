551b88c5fdf2f7b8afdb5442aec2a7df022f4934
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-06-20 19:36:58 +0900

    modified

--- unit_test/sensor_test/ballo_test.py
+++ unit_test/sensor_test/ballo_test.py
@@ -1,3 +1,36 @@
+# import asyncio
+# import time
+# from mavsdk import System
+
+                
+                
+# async def run():
+
+#     print("Hey")
+#     drone = System()
+    
+#     print("Waiting for drone to connect...")
+#     await drone.connect(system_address="serial:///dev/ttyACM0:115200")
+
+
+#     async for state in drone.core.connection_state():
+#         if state.is_connected:
+#             print(f"-- Connected to drone!")
+#             break
+        
+#     asyncio.ensure_future(print_pressure(drone))
+    
+#     while True:
+#         await asyncio.sleep(1)
+        
+# async def print_pressure(drone):
+#     async for pressure in drone.telemetry.scaled_pressure():
+#         print(pressure)
+
+# if __name__ == "__main__":
+#     # Run the asyncio loop
+#     asyncio.run(run())
+
 import asyncio
 import time
 from mavsdk import System
@@ -18,12 +51,6 @@ async def run():
             print(f"-- Connected to drone!")
             break
         
-    asyncio.ensure_future(print_pressure(drone))
-    
-    while True:
-        await asyncio.sleep(1)
-        
-async def print_pressure(drone):
     async for pressure in drone.telemetry.scaled_pressure():
         print(pressure)
 
