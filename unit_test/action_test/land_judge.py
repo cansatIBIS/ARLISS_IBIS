0eb6d0f3c74dd0bcdd62c18bdb3ddc1e0ef173fb
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-06-23 17:10:50 +0900

    modified

--- unit_test/action_test/land_judge.py
+++ unit_test/action_test/land_judge.py
@@ -2,7 +2,20 @@ import numpy as np
 import scipy.stats as stats
 import asyncio
 from mavsdk import System
-from logger import logger_info, logger_debug
+from logger import logger_info
+
+
+async def run():
+    drone = System()
+    print("--Waiting for drone to connected...")
+    await drone.connect(system_address="serial:///dev/ttyACM0:115200")
+    
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print(f"-- Connected to drone!")
+            logger_info.info("-- Connected to drone!")
+            break
+    land_judge()
 
 
 async def land_judge(drone):
@@ -52,63 +65,4 @@ async def smirnov_grubbs(data, alpha):
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(run())
-
-
-# altitude = 2.5
-
-# async def run():
-#     drone = System()
-#     logger_info.info("Waiting for drone to connect...")
-#     async for state in drone.core.connection_state():
-#         if state.is_connected:
-#             print(f"-- Connected to drone!")
-#             logger_info.info("-- Connected to drone!")
-#             break
-        
-#     print_altitude_task = asyncio.create_task(print_altitude(drone))
-#     print_flight_mode_task = asyncio.create_task(print_flight_mode(drone))
-#     arm_takeoff_task = asyncio.create_task(arm_takeoff_land(drone))
-#     land_judge_task = asyncio.create_task(land_judge(drone))
-#     await print_altitude_task
-#     await print_flight_mode_task
-#     await arm_takeoff_task
-#     await land_judge_task
     
-
-# async def arm_takeoff_land(drone):
-#     print("-- Arming")
-#     logger_info.info("-- Arming")
-#     await drone.action.arm()
-#     print("-- Armed")
-#     logger_info.info("-- Armed")
-#     print("-- Taking off")
-#     logger_info.info("-- Taking off")
-#     await drone.action.set_takeoff_altitude(altitude)
-#     await drone.action.takeoff()
-
-#     await asyncio.sleep(15)
-
-#     print("-- Landing")
-#     logger_info.info("-- Landing")
-#     await drone.action.land()
-
-
-# async def print_altitude(drone):
-#     previous_altitude = 0.0
-    
-#     async for distance in drone.telemetry.distance_sensor():
-#         altitude_now = distance.current_distance_m
-#         print("difference : {}".format(altitude_now - previous_altitude))
-#         if abs(previous_altitude - altitude_now) >= 0.1:
-#             previous_altitude = altitude_now
-#             print(f"Altitude: {altitude_now}")
-
-#             logger_info.info(f"mode:{mode} lidar:{altitude_now}m")
-
-
-# async def print_flight_mode(drone):
-#     global mode
-#     async for flight_mode in drone.telemetry.flight_mode():
-#         mode = flight_mode
-        
-
