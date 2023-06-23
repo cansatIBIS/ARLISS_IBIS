f3520f4465df5e2042b2044d52b5c7ac33497046
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-06-23 17:07:02 +0900

    modified

--- unit_test/action_test/land_judge.py
+++ unit_test/action_test/land_judge.py
@@ -1,22 +1,53 @@
+import numpy as np
+import scipy.stats as stats
 import asyncio
 from mavsdk import System
 from logger import logger_info, logger_debug
 
 
 async def land_judge(drone):
+    is_landed = True
     async for distance in drone.telemetry.distance_sensor():
-        d = distance.current_distance_m
-        asyncio.sleep(1)
+        if low_alt_judge(distance.current_distance_m):
+            true_distance = smirnov_grubbs(alt_list(drone))[0]
+            num = len(true_distance)
+            ave = sum(true_distance)/num
+            for i in range(num):
+                if abs(ave-true_distance[i]) > 0.01:
+                    is_landed = False
+            if is_landed:
+                print("--Landed")
         
         
-async def alt_change(drone):
+async def low_alt_judge(alt):
+    if alt <= 1:
+        return True
+        
+        
+async def alt_list(drone):
     distance_list = []
     iter = 0
     async for distance in drone.telemetry.distance_sensor():
         iter += 1
         distance_list.append(distance.current_distance_m)
-        if iter >= 9:
+        if iter >= 100:
             break
+    return distance_list
+        
+
+async def smirnov_grubbs(data, alpha):
+	x, o = list(data), []
+	while True:
+		n = len(x)
+		t = stats.t.isf(q=(alpha / n) / 2, df=n - 2)
+		tau = (n - 1) * t / np.sqrt(n * (n - 2) + n * t * t)
+		i_min, i_max = np.argmin(x), np.argmax(x)
+		myu, std = np.mean(x), np.std(x, ddof=1)
+		i_far = i_max if np.abs(x[i_max] - myu) > np.abs(x[i_min] - myu) else i_min
+		tau_far = np.abs((x[i_far] - myu) / std)
+		if tau_far < tau: break
+		o.append(x.pop(i_far))
+	return (np.array(x), np.array(o))
 
 
 if __name__ == "__main__":
