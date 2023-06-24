26f881f3e3a25f51c3cb42895325f738115268f5
laika90 <momoshinak46@gmail.com>
2023-06-24 21:01:47 +0900

    [Modify] arm

--- unit_test/action_test/arm_test.py
+++ unit_test/action_test/arm_test.py
@@ -7,22 +7,12 @@ async def run():
     drone = System()
     print("HEY")
     await drone.connect(system_address="serial:///dev/ttyACM0:115200")
-    # await drone.connect(system_address="udp://:14540")
-
-    
 
     print("Waiting for drone to connect...")
     async for state in drone.core.connection_state():
         if state.is_connected:
             print("-- Connected to drone!")
             break
-        
-    # gps_info_0 = drone.telemetry.gps_info()
-    # while True:
-    #     gps_info = drone.telemetry.gps_info()
-    #     if gps_info != gps_info_0:
-    #         break
-    
 
     print("Waiting for drone to have a global position estimate...")
     async for health in drone.telemetry.health():
@@ -35,20 +25,7 @@ async def run():
     print("-- arming")
     await drone.action.arm()
     
-    # drone.is_armed = False
-    # drone.is_armed = drone.telemetry.Telemetry.armed()
-    
     print("-- Armed")
-
-    # print("-- Taking off")
-    # await drone.action.takeoff()
-
-    # await asyncio.sleep(10)
-
-    # print("-- Landing")
-    # await drone.action.land()
-
-    # status_text_task.cancel()
 
 if __name__ == "__main__":
     # Run the asyncio loop
--- unit_test/action_test/arm_without_gps_with_log.py
+++ unit_test/action_test/arm_without_gps_with_log.py
@@ -19,12 +19,7 @@ async def run():
         if state.is_connected:
             print(f"-- Connected to drone!")
             break
-
-    # print("Waiting for drone to have a global position estimate...")
-    # async for health in drone.telemetry.health():
-    #     if health.is_global_position_ok and health.is_home_position_ok:
-    #         print("-- Global position estimate OK")
-    #         break
+        
     logger_info.info("-- Arming")
     print("-- Arming")
     await drone.action.arm()
