5533e7096f388458a1ac85dcc473809eecb49f25
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-06-26 15:05:07 +0900

    modified

--- IB/unit_test/action_test/fusing_test.py
+++ IB/unit_test/action_test/fusing_test.py
@@ -6,21 +6,29 @@ import RPi.GPIO as GPIO
 
 def fusing():
     # BCM(GPIO番号)で指定する設定
-    GPIO.setmode(GPIO.BCM)
+    try:
+        GPIO.setmode(GPIO.BCM)
 
-    # GPIO26を出力モード設定
-    GPIO.setup(26, GPIO.OUT)
+        # GPIO26を出力モード設定
+        GPIO.setup(26, GPIO.OUT)
 
-    # GPIO26の出力を1にして、LED点灯
-    GPIO.output(26, GPIO.OUT)
-    print("-- Fusing")
+        # GPIO26の出力を1にして、LED点灯
+        GPIO.output(26, 1)
+        print("-- Fusing")
 
-    # 0.5秒待つ
-    time.sleep(5.0)
-    print("-- Fused! Please Fly")
+        # 0.5秒待つ
+        time.sleep(5.0)
+        print("-- Fused! Please Fly")
 
-    # GPIO17の出力を0にして、LED消灯
-    GPIO.output(26, GPIO.OUT)
+        # GPIO17の出力を0にして、LED消灯
+        GPIO.output(26, 0)
+        
+        GPIO.cleanup()
+        
+    
+    except KeyboardInterrupt:
+        # GPIO設定クリア
+        GPIO.cleanup()
 
 if __name__ == "__main__":
     fusing()
