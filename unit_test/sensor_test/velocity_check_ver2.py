383d786aa4c218381f2adbe9255247352b767425
hookunsencho <namiharrypotter@yahoo.co.jp>
2023-06-26 14:39:55 +0900

    modified

--- unit_test/sensor_test/velocity_check_ver2.py
+++ unit_test/sensor_test/velocity_check_ver2.py
@@ -5,7 +5,11 @@ from mavsdk import System
 import time
 import datetime
 import csv
+import math as m
 
+vx_list=[]
+vy_list=[]
+vz_list=[]
 v_list=[]
 pb_list = []
 async def run():
@@ -13,14 +17,16 @@ async def run():
     # Init the drone
     drone = System()
     await drone.connect(system_address="serial:///dev/ttyACM0:115200")
-    print("conected to drone")
+    print("connected to drone")
 
 
     while True:
         async for v in drone.telemetry.velocity_ned():
-            v_list.append(v.down_m_s)
-            print(type(v.down_m_s))
-            print(f"velocity:{v.down_m_s}")
+            vx_list.append(v.north_m_s)
+            vy_list.append(v.east_m_s)
+            vz_list.append(v.down_m_s)
+            v_list.append(m.sqrt((v.north_m_s)**2+(v.east_m_s)**2+(v.down_m_s)**2))
+            # print(f"velocity:{v.down_m_s}")
             break
         async for o in drone.telemetry.odometry():
             pb = o.position_body
@@ -31,7 +37,7 @@ async def run():
 
         now = time.time()
         print(now-start)
-        if now-start>60:
+        if now-start>30:
             break
 
 
@@ -39,6 +45,9 @@ async def run():
     dt_now = datetime.datetime.now()
     with open(f"/home/pi/ARLISS_IBIS/log/log_csv/velocity_check_ver2 {dt_now}.csv","w") as file:
         writer = csv.writer(file)
+        writer.writerow(vx_list)
+        writer.writerow(vy_list)
+        writer.writerow(vz_list)
         writer.writerow(v_list)
 
 
