3e9263ed67deffbdebd5bc93214d7daa67371b77
hookunsencho <namiharrypotter@yahoo.co.jp>
2023-06-27 17:08:29 +0900

    modified

--- unit_test/sensor_test/parachute_graph.py
+++ unit_test/sensor_test/parachute_graph.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-df = pd.read_csv('../../log/log_csv/velocity_check_ver2 2023-06-26 16_25_34.260847.csv',header=None).T
-df.columns = ["v_x","v_y","v_z","v","x","y","z","roll","pitch","yaw"]
+df = pd.read_csv('/Users/fukudakazuya/ARLISS_IBIS/log/log_csv/velocity_check_ver2 2023-06-26 16:25:34.260847.csv',header=None).T
+# df.columns = ["v_x","v_y","v_z","v","x","y","z","roll","pitch","yaw"]
+df.columns = ["v_x","v_y","v_z","v"]
 df[["v_x","v_y","v_z","v"]].plot()
 # df[["x"]].plot()
 # df[["y"]].plot()
 # df[["z"]].plot()
-df[["x","y","z"]].plot(subplots=True)
-df[["roll","pitch","yaw"]].plot(subplots=True)
+# df[["x","y","z"]].plot(subplots=True)
+# df[["roll","pitch","yaw"]].plot(subplots=True)
+plt.show()
