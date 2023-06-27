68e39c647c03d215c697dba7a50e9f45c7f586d9
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-06-27 16:19:58 +0900

    [CREATE] take_pic.py

---
+++ IB/unit_test/sensor_test/take_pic.py
@@ -0,0 +1,25 @@
+import picamera
+import cv2
+import datetime
+
+
+
+class Camera:
+    
+    def __init__(self):
+        self.camera = picamera.PiCamera()
+        print('キャメラ初期化完了')
+    
+    def take_pic(self, file_path):
+        self.camera.capture(file_path)
+        
+    
+if __name__ == "__main__":
+    camera = Camera()
+    
+    file_path = '/home/pi/utat/img/image{:>03d}.jpg'.format(test)
+
+    print("taking pic...: {}".format(file_path))
+    camera.take_pic(file_path) # 写真を撮る
+    
+    cv2.destroyAllWindows()
