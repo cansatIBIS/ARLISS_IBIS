272080217e095525cd8e5613e0cec7820f4c157f
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-07-03 18:05:03 +0900

    modified

--- IB/unit_test/sensor_test/lidar_error.py
+++ IB/unit_test/sensor_test/lidar_error.py
@@ -21,7 +21,7 @@ async def run():
     try:
         await asyncio.wait_for(get_alt(drone), timeout = 3)
     except asyncio.TimeoutError as e:
-        print(str(e))
+        print("timeout")
     
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(run())
