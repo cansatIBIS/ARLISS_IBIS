9898529e895a85a4788719e77160f7c53d68a07a
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-08-01 15:35:34 +0900

    modified

--- IB/unit_test/lora/lora_server.py
+++ IB/unit_test/lora/lora_server.py
@@ -20,57 +20,57 @@ while True:
         break
 print ("Serial port OK. Waiting for reset.")
 
-while True:
-    buf = ser.readline()
-    if buf == b'': 
-        # print("nanimonaiyo")
-        continue
-    print (buf.decode('utf-8','replace').strip())
-    if buf == b"Select Mode [1.terminal or 2.processor]\r\n": 
-        break
-ser.write(b'1\r\n')
+# while True:
+#     buf = ser.readline()
+#     if buf == b'': 
+#         # print("nanimonaiyo")
+#         continue
+#     print (buf.decode('utf-8','replace').strip())
+#     if buf == b"Select Mode [1.terminal or 2.processor]\r\n": 
+#         break
+# ser.write(b'1\r\n')
 
-
-while True:
-    buf = ser.readline()
-    if buf == b'': 
-        continue
-    print (buf.decode().strip())
-    if buf.decode().find('LORA')!=-1: 
-        break
-ser.write(b'z\r\n')
-
-while True:
-    buf = ser.readline()
-    if buf == b'': break
-    print (buf.decode().strip())
-
-print("Ready.")
-
-while True:
-    
-    bufw=input()
-    ser.write((bufw+"\r\n").encode())
-
-    # bufr = ser.readline()
-    # if bufr == b'': 
-    #     continue
-    # print(bufr)
 
 # while True:
 #     buf = ser.readline()
 #     if buf == b'': 
 #         continue
-#     m=re.search(r"(-[0-9.]+).*Data\((.*)\)", buf.decode());
-#     if m:
-#         time.sleep(0.1)
-#         buf=m.group(2) + " (" + m.group(1) + " dBm)"
-#         print("send: " + buf)
-#         ser.write((buf+"\r\n").encode())
-#         if m.group(2) == 'quit':
-#             print("Bye.")
-#             break
+#     print (buf.decode().strip())
+#     if buf.decode().find('LORA')!=-1: 
+#         break
+# ser.write(b'z\r\n')
 
-# ser.close()
+# while True:
+#     buf = ser.readline()
+#     if buf == b'': break
+#     print (buf.decode().strip())
+
+# print("Ready.")
+
+# while True:
+    
+#     bufw=input()
+#     ser.write((bufw+"\r\n").encode())
+
+#     bufr = ser.readline()
+#     if bufr == b'': 
+#         continue
+#     print(bufr)
+
+while True:
+    buf = ser.readline()
+    if buf == b'': 
+        continue
+    m=re.search(r"(-[0-9.]+).*Data\((.*)\)", buf.decode());
+    if m:
+        time.sleep(0.1)
+        buf=m.group(2) + " (" + m.group(1) + " dBm)"
+        print("send: " + buf)
+        ser.write((buf+"\r\n").encode())
+        if m.group(2) == 'quit':
+            print("Bye.")
+            break
+
+ser.close()
 
 # EOF
