20004a2f1183917de4c9d88fcf2cb90366a6e0bf
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-08-02 11:55:39 +0900

    create lora_lidar_2.py

---
+++ IB/unit_test/lora/lora_lidar_2.py
@@ -0,0 +1,31 @@
+import serial
+import time
+from mavsdk import System
+import asyncio
+
+system_address = "serial:///dev/ttyACM0:115200"
+
+async def main():
+    drone = System()
+    print("Waiting for drone to connect...")
+    await drone.connect(system_address)
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print(f"-- Connected to drone!")
+            break
+    while True:
+        try:
+            ser = serial.Serial('/dev/ttyAMA0' ,19200,timeout=1)
+        except:
+            print ("Serial port error. Waiting.")
+            time.sleep(5)
+        else:
+            break
+    print ("Serial port OK.")
+    ser.write(b'1\r\n')
+    time.sleep(1)
+    ser.write(b'z\r\n')
+    time.sleep(1)
+    ser.write(("Let's GO\r\n").encode())
+    print("READY")
+    return ser
