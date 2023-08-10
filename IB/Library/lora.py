c1567340e6ad6dbe1a0932abc494f160fdea92c7
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-08-10 18:03:41 +0900

    modified

--- IB/Library/lora.py
+++ IB/Library/lora.py
@@ -10,44 +10,52 @@ import time
 
 class Lora:
     def __init__(self):
-        self.rst = 17
-        self.power = 4
+        self.power = 7
         self.CRLF = "\r\n"
-        self.serial = serial.Serial("/dev/ttyS0", 115200, timeout=1)
-        self.is_on = False
-        self.is_out = False
+        self.is_power_on = False
+        self.connect_counter = 0
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
         GPIO.setup(self.rst, GPIO.OUT)
         GPIO.setup(self.power, GPIO.OUT)
         
-    async def power_off(self):
-        """cut power for lora"""
-        GPIO.output(self.power, GPIO.LOW)
-        self.is_on = False
-        await asyncio.sleep(1)
-
-    async def power_on(self):
-        """start lora"""
-        GPIO.output(self.power, GPIO.HIGH)
-
-        GPIO.output(self.rst, GPIO.LOW)
-        await asyncio.sleep(2)
-        GPIO.output(self.rst, GPIO.HIGH)
-        await asyncio.sleep(2)
-        print("lora power on")
-        await self.write("processor")
-        await self.write("start")
-
-        self.is_on = True
-        
     def Serial_Connect(self):
         while True:
             try:
-                ser = serial.Serial('/dev/ttyS0', 115200, timeout=3)
+                self.serial = serial.Serial("/dev/ttyS0", 115200, timeout=1)
             except:
                 print ("Serial port error. Waiting.")
-                time.sleep(5)
+                self.connect_counter += 1
+                if self.connect_counter == 100:
+                    break
+                time.sleep(3)
             else:
                 break
         print ("Serial port OK.")
+        self.write(b'2'+self.CRLF)
+        time.sleep(1)
+        self.write(b'z'+self.CRLF)
+        time.sleep(1)
+        self.write(("Lora start"+self.CRLF).encode())
+        print("Lora READY")
+       
+    async def power_off(self):
+        GPIO.output(self.power, GPIO.LOW)
+        self.is_power_on = False
+        print("Lora power off")
+        await asyncio.sleep(1)
+
+    async def power_on(self):
+        GPIO.output(self.power, GPIO.HIGH)
+        print("Lora power on")
+        await self.write("processor")
+        await self.write("start")
+
+        self.is_power_on = True
+        
+    async def start_communication(self):
+    
+    async def write(self, message: str):
+        msg_send = str(message) + self.CRLF
+        self.serial.write(msg_send.encode("ascii"))
+        await asyncio.sleep(4)
