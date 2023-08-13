fc3384aef975ac76fce8542f08759b4bcc4926bc
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-08-13 16:43:44 +0900

    modified

--- IB/unit_test/action_test/judge_takeoff.py
+++ IB/unit_test/action_test/judge_takeoff.py
@@ -11,7 +11,7 @@ from logger import logger_info
  
 light_threshold = 250
 is_landed = False
-PIN = 5
+fuse_Pin = 26
 store_timelimit = 10
 release_timelimit = 10
 land_timelimit = 10
@@ -205,18 +205,18 @@ def fusing():
         logger_info.info("-- Fuse start")
         GPIO.setmode(GPIO.BCM)
 
-        GPIO.setup(PIN, GPIO.OUT)
+        GPIO.setup(fuse_Pin, GPIO.OUT)
 
-        GPIO.output(PIN, 0)
+        GPIO.output(fuse_Pin, 0)
         logger_info.info("-- Fusing")
 
         time.sleep(5.0)
         logger_info.info("-- Fused! Please Fly")
 
-        GPIO.output(PIN, 1)
+        GPIO.output(fuse_Pin, 1)
     
     except:
-        GPIO.output(PIN, 1)
+        GPIO.output(fuse_Pin, 1)
 
 
 def judge():
