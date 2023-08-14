a2c7c08dc517800fa481ae9e221b753f46db0c5a
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-08-14 18:58:52 +0900

    modified

--- IB/Performance/NSE.py
+++ IB/Performance/NSE.py
@@ -102,9 +102,9 @@ async def send_gps(lora, drone):
         lat = "lat:" + str(lat_deg)
         lng = "lng:" + str(lng_deg)
         alt = "alt:" + str(alt_deg)
-        await lora.write(lat.encode())
-        await lora.write(lng.encode())
-        await lora.write(alt.encode())
+        await write(lora, lat.encode())
+        await write(lora, lng.encode())
+        await write(lora, alt.encode())
     await asyncio.sleep(5)
     
     
