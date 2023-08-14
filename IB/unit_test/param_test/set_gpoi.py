62b6543daaa94bbe874d0c3dcc31fd3287346785
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-08-14 18:00:12 +0900

    modified

--- IB/unit_test/param_test/set_gpoi.py
+++ IB/unit_test/param_test/set_gpoi.py
@@ -4,7 +4,7 @@ import RPi.GPIO as GPIO
 SWITCH_PIN = 4 # ピン番号
 
 GPIO.setmode(GPIO.BCM)
-# GPIO.setup(SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)   # プルアップの場合
-GPIO.setup(SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # プルダウンの場合
+GPIO.setup(SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)   # プルアップの場合
+# GPIO.setup(SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # プルダウンの場合
 
 result = GPIO.input(SWITCH_PIN) # ピンの値を読み取る(HIGH or LOWの1 or 0)
