466457d526913a7da26ec729beaabe81b7e71c41
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-08-14 12:24:30 +0900

    modified

--- IB/E2E/matsudo_E2E_offboard.py
+++ IB/E2E/matsudo_E2E_offboard.py
@@ -269,7 +269,7 @@ async def run():
             break
         
     get_log_task = asyncio.ensure_future(get_log(drone))
-    img_navigation_task = asyncio.ensure_future(img_navigation(drone))
+    # img_navigation_task = asyncio.ensure_future(img_navigation(drone))
 
     mission_items = []
     mission_items.append(MissionItem(goal[0],
@@ -354,7 +354,14 @@ async def run():
     await drone.mission.start_mission()
 
     await get_log_task
-    await img_navigation_task
+    # await img_navigation_task
+    while True:
+        await asyncio.sleep(1)
+        mission_finished = await drone.mission.is_mission_finished()
+        if mission_finished:
+            break
+        
+    await drone.action.land()
   
 async def get_log(drone):
     while True:
@@ -376,125 +383,125 @@ async def get_log(drone):
         logger_info.info(str(log_txt))
         await asyncio.sleep(0.5)
 
-async def img_navigation(drone):
-    while True:
-        await asyncio.sleep(1)
-        mission_finished = await drone.mission.is_mission_finished()
-        if mission_finished:
-            logger_info.info("mission finished")
-            break
+# async def img_navigation(drone):
+#     while True:
+#         await asyncio.sleep(1)
+#         mission_finished = await drone.mission.is_mission_finished()
+#         if mission_finished:
+#             logger_info.info("mission finished")
+#             break
 
-    await asyncio.sleep(10)
-
-
-    camera = picamera.PiCamera()
-    logger_info.info('キャメラ初期化完了')
-
-    async for d in drone.telemetry.distance_sensor(): #? 測れなかったらどうしよう
-        lidar_height = d.current_distance_m
-        logger_info.info(f"current height:{lidar_height}m")
-        break
-    async for heading in drone.telemetry.heading():
-        heading_deg = heading.heading_deg
-        logger_info.info(f"current heading: {heading_deg}") 
-        break
+#     await asyncio.sleep(10)
 
 
-    file_path = '/home/pi/ARLISS_IBIS/Images/image_navigation_test_ver2_{}.jpg'.format(datetime.datetime.now())
+#     camera = picamera.PiCamera()
+#     logger_info.info('キャメラ初期化完了')
 
-    logger_info.info("taking pic...: {}".format(file_path))
-    take_pic(camera,file_path) # 写真を撮る
-    res = detect_center(file_path) # 赤の最大領域の占有率と重心を求める
+#     async for d in drone.telemetry.distance_sensor(): #? 測れなかったらどうしよう
+#         lidar_height = d.current_distance_m
+#         logger_info.info(f"current height:{lidar_height}m")
+#         break
+#     async for heading in drone.telemetry.heading():
+#         heading_deg = heading.heading_deg
+#         logger_info.info(f"current heading: {heading_deg}") 
+#         break
 
-    logger_info.info('percent={}, center={}'.format(res['percent'], res['center']))
 
-    await asyncio.sleep(1)
+#     file_path = '/home/pi/ARLISS_IBIS/Images/image_navigation_test_ver2_{}.jpg'.format(datetime.datetime.now())
 
-    distance = lidar_height
-    a = pixel_number_x*pixel_size/1000 # 画像(ピクセル単位)の横の長さ[mm]
-    b = pixel_number_y*pixel_size/1000 # 画像(ピクセル単位)の縦の長さ[mm]
-    image_x = distance*a/f # 画像の横の距離[m]
-    image_y = distance*b/f # 画像の縦の距離[m]
-    x_m = res['center'][0]*image_x/2
-    y_m = res['center'][1]*image_y/2
+#     logger_info.info("taking pic...: {}".format(file_path))
+#     take_pic(camera,file_path) # 写真を撮る
+#     res = detect_center(file_path) # 赤の最大領域の占有率と重心を求める
 
-    east_m = 1/np.sqrt(2)*(y_m-x_m)*np.cos(heading*np.pi/180)-1/np.sqrt(2)*(y_m+x_m)*np.sin(heading*np.pi/180)
-    north_m = 1/np.sqrt(2)*(y_m-x_m)*np.sin(heading*np.pi/180)+1/np.sqrt(2)*(y_m+x_m)*np.cos(heading*np.pi/180)
+#     logger_info.info('percent={}, center={}'.format(res['percent'], res['center']))
 
-    logger_info.info(f"go to the red position:北に{north_m}m,東に{east_m}")
+#     await asyncio.sleep(1)
 
-    logger_info.info("-- Setting initial setpoint")
-    await drone.offboard.set_position_ned(PositionNedYaw(0.0, 0.0, 0.0 , 0.0))
-    await drone.offboard.start()
+#     distance = lidar_height
+#     a = pixel_number_x*pixel_size/1000 # 画像(ピクセル単位)の横の長さ[mm]
+#     b = pixel_number_y*pixel_size/1000 # 画像(ピクセル単位)の縦の長さ[mm]
+#     image_x = distance*a/f # 画像の横の距離[m]
+#     image_y = distance*b/f # 画像の縦の距離[m]
+#     x_m = res['center'][0]*image_x/2
+#     y_m = res['center'][1]*image_y/2
 
-    await drone.offboard.set_position_ned(
-            PositionNedYaw(north_m, east_m, 0.0, 0.0)) #? 方位が違うかも
-    await asyncio.sleep(10)
+#     east_m = 1/np.sqrt(2)*(y_m-x_m)*np.cos(heading*np.pi/180)-1/np.sqrt(2)*(y_m+x_m)*np.sin(heading*np.pi/180)
+#     north_m = 1/np.sqrt(2)*(y_m-x_m)*np.sin(heading*np.pi/180)+1/np.sqrt(2)*(y_m+x_m)*np.cos(heading*np.pi/180)
 
-    logger_info.info("-- Stopping offboard")
-    try:
-        await drone.offboard.stop()
-    except OffboardError as error:
-        logger_info.info(f"Stopping offboard mode failed \
-                with error code: {error._result.result}")
-    logger_info.info("画像認識成功、着陸します") 
-    await drone.action.land()
+#     logger_info.info(f"go to the red position:北に{north_m}m,東に{east_m}")
 
-def take_pic(camera,file_path):
-    camera.capture(file_path)
+#     logger_info.info("-- Setting initial setpoint")
+#     await drone.offboard.set_position_ned(PositionNedYaw(0.0, 0.0, 0.0 , 0.0))
+#     await drone.offboard.start()
 
-def save_detected_img(file_path, img, center_px):
-    cv2.circle(img, (int(center_px[0]), int(center_px[1])), 30, (0, 200, 0),
-            thickness=3, lineType=cv2.LINE_AA)
-    cv2.imwrite(file_path, img)
+#     await drone.offboard.set_position_ned(
+#             PositionNedYaw(north_m, east_m, 0.0, 0.0)) #? 方位が違うかも
+#     await asyncio.sleep(10)
 
-def detect_center(file_path):
-    img = cv2.imread(file_path) # 画像を読み込む
+#     logger_info.info("-- Stopping offboard")
+#     try:
+#         await drone.offboard.stop()
+#     except OffboardError as error:
+#         logger_info.info(f"Stopping offboard mode failed \
+#                 with error code: {error._result.result}")
+#     logger_info.info("画像認識成功、着陸します") 
+#     await drone.action.land()
+
+# def take_pic(camera,file_path):
+#     camera.capture(file_path)
+
+# def save_detected_img(file_path, img, center_px):
+#     cv2.circle(img, (int(center_px[0]), int(center_px[1])), 30, (0, 200, 0),
+#             thickness=3, lineType=cv2.LINE_AA)
+#     cv2.imwrite(file_path, img)
+
+# def detect_center(file_path):
+#     img = cv2.imread(file_path) # 画像を読み込む
     
-    height, width = img.shape[:2] # 画像のサイズを取得する
+#     height, width = img.shape[:2] # 画像のサイズを取得する
 
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) # 色基準で2値化する
+#     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) # 色基準で2値化する
 
-    # 色の範囲を指定する
-    hsv_min = np.array([0,145,0])
-    hsv_max = np.array([5,255,255])
-    mask1 = cv2.inRange(hsv, hsv_min, hsv_max)
+#     # 色の範囲を指定する
+#     hsv_min = np.array([0,145,0])
+#     hsv_max = np.array([5,255,255])
+#     mask1 = cv2.inRange(hsv, hsv_min, hsv_max)
 
-    # 赤色のHSVの値域2
-    hsv_min = np.array([0,110,0]) #カメラ故障のため，0→150へ変更
-    hsv_max = np.array([179,255,255])
-    mask2 = cv2.inRange(hsv, hsv_min, hsv_max)
+#     # 赤色のHSVの値域2
+#     hsv_min = np.array([0,110,0]) #カメラ故障のため，0→150へ変更
+#     hsv_max = np.array([179,255,255])
+#     mask2 = cv2.inRange(hsv, hsv_min, hsv_max)
 
-    mask = mask1 + mask2
+#     mask = mask1 + mask2
 
-    # 非ゼロのピクセルが連続してできた領域を検出する
-    nlabels, labels, stats, centroids = cv2.connectedComponentsWithStats(mask)
+#     # 非ゼロのピクセルが連続してできた領域を検出する
+#     nlabels, labels, stats, centroids = cv2.connectedComponentsWithStats(mask)
 
-    #　画像の背景の番号は 0 とラベリングされているので、実際のオブジェクトの数は nlabels - 1 となる
-    nlabels = nlabels - 1
-    labels = np.delete(labels, obj=0, axis=0)
-    stats = np.delete(stats, obj=0, axis=0)
-    centroids = np.delete(centroids, obj=0, axis=0)
-    centroids[:,0] = (width/2 - centroids[:,0]) / width*2
-    centroids[:,1] = (height/2 - centroids[:,1]) / height*2
-    percent = stats[:,4] / (height*width)
+#     #　画像の背景の番号は 0 とラベリングされているので、実際のオブジェクトの数は nlabels - 1 となる
+#     nlabels = nlabels - 1
+#     labels = np.delete(labels, obj=0, axis=0)
+#     stats = np.delete(stats, obj=0, axis=0)
+#     centroids = np.delete(centroids, obj=0, axis=0)
+#     centroids[:,0] = (width/2 - centroids[:,0]) / width*2
+#     centroids[:,1] = (height/2 - centroids[:,1]) / height*2
+#     percent = stats[:,4] / (height*width)
     
-    res = {}
+#     res = {}
 
-    if nlabels == 0:
-        res['height'] = None
-        res['width'] = None
-        res['percent'] = 0
-        res['center'] = None
-    else:
-        max_index = np.argmax(percent)
-        res['height'] = height
-        res['width'] = width
-        res['percent'] = percent[max_index]
-        res['center'] = centroids[max_index]
-        save_detected_img(file_path, img, ((1-res['center'][0])*width/2, (1-res['center'][1])*height/2))
+#     if nlabels == 0:
+#         res['height'] = None
+#         res['width'] = None
+#         res['percent'] = 0
+#         res['center'] = None
+#     else:
+#         max_index = np.argmax(percent)
+#         res['height'] = height
+#         res['width'] = width
+#         res['percent'] = percent[max_index]
+#         res['center'] = centroids[max_index]
+#         save_detected_img(file_path, img, ((1-res['center'][0])*width/2, (1-res['center'][1])*height/2))
     
-    return res
+#     return res
     
     
 if __name__ == "__main__":
