93a520bdfc769701c71df94dc2822e0a889e606c
hookunsencho <namiharrypotter@yahoo.co.jp>
2023-08-16 18:16:09 +0900

    modified

--- unit_test/action_test/mission_2_waypoints.py
+++ unit_test/action_test/mission_2_waypoints.py
@@ -66,7 +66,7 @@ async def run():
     
 
     
-    waypoint1 = [35.7961963, 139.8918611]
+    waypoint1 = [39.8847861, 139.85537]
     # waypoint2 = [center[0] + lat_deg_per_m * south_m, center[1]]
     mission_items = []
     mission_items.append(MissionItem(waypoint1[0],
