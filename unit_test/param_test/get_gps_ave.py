6d7d520ce49e81fd86cea13ab980751ca72bbd52
hookunsencho <namiharrypotter@yahoo.co.jp>
2023-08-17 15:29:41 +0900

    modified

--- unit_test/param_test/get_gps_ave.py
+++ unit_test/param_test/get_gps_ave.py
@@ -10,14 +10,17 @@ async def run():
     drone = System()
     await drone.connect(system_address="serial:///dev/ttyACM0:115200")
 
-    lat_lst = np.empty(0)
-    lng_lst = np.empty(0)
+    lat_lst = []
+    lng_lst = []
     for _ in range(10):
         lat, lng = await get_gps(drone)
         print(f"lat:{lat},lng:{lng}")
         lat_lst.append(lat)
         lng_lst.append(lng)
-    print(f"lat_ave:{np.average(lat_lst)},lng_ave:{np.average(lng_lst)}")
+    lat_l = np.array(lat_lst)
+    lng_l = np.array(lng_lst)
+
+    print(f"lat_ave:{np.average(lat_l)},lng_ave:{np.average(lng_l)}")
     
 
 
