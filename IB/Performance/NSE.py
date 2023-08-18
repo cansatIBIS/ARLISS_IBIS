f44886cf9c45034db37249d5f5ebcfdb7d200bae
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-08-18 22:06:59 +0900

    modified

--- IB/Performance/NSE.py
+++ IB/Performance/NSE.py
@@ -20,14 +20,14 @@ lora_power_pin = 4
 # ----------------------------------------
 
 #　Time settings--------------------------
-wait_time = 60
+wait_time = 1
 lora_sleep_time = 3
-fuse_time = 5.0
-stored_timelimit = 60
-stored_judge_time = 15
-released_timelimit = 3 * 60
+fuse_time = 3.0
+stored_timelimit = 15
+stored_judge_time = 5
+released_timelimit = 15
 released_judge_time = 5
-land_timelimit = 20
+land_timelimit = 10
 # ----------------------------------------
 
 #　flags--------------------------
