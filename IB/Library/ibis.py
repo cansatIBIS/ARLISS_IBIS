a07408627b0b7de3d5c4642518e2c774d633de1d
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-08-24 20:43:48 +0900

    modified

--- IB/Library/ibis.py
+++ IB/Library/ibis.py
@@ -6,6 +6,7 @@ import asyncio
 class Ibis:
     
     def __init__(self,
+                 
                  fuse_PIN,
                  wait_time,
                  lora_sleep_time, 
@@ -16,12 +17,15 @@ class Ibis:
                  longitude_deg,
                  max_speed,
                  lidar,
+                 
                  light_threshold,
                  stored_timelimit,
                  stored_judge_time,
                  released_timelimit,
                  released_judge_time,
+                 
                  lora_power_Pin,
+                 
                  deamon_file = open("/home/pi/ARLISS_IBIS/IB/log/Performance_log.txt")):
         
         self.pixhawk = Pixhawk(fuse_PIN,
@@ -42,6 +46,10 @@ class Ibis:
                       released_judge_time)
         
         self.lora = Lora(lora_power_Pin)
+        
+        
+        self.deamon_file = deamon_file
+        self.deamon_log = self.deamon_file.read()
         
     async def MATSUDO():
         return
