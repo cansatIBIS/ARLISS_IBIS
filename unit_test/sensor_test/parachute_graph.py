340259dfe7e3e4d7ac40a09b4ba915f90a1594ce
hookunsencho <namiharrypotter@yahoo.co.jp>
2023-08-26 10:40:59 +0900

    modified

--- unit_test/sensor_test/parachute_graph.py
+++ unit_test/sensor_test/parachute_graph.py
@@ -6,5 +6,5 @@ df.columns = ["v_x","v_y","v_z","v","x","y","z","roll","pitch","yaw","forward_a"
 df[["v_x","v_y","v_z","v"]].plot()
 df[["x","y","z"]].plot(subplots=True)
 df[["roll","pitch","yaw"]].plot(subplots=True)
-df[["forward_a","right_a","down_a","acc"]].plot(subplots=True)
+df[["forward_a","right_a","down_a","acc"]].plot()
 plt.show()
