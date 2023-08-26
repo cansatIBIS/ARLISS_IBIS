63ae650ebd84c217aabdb540daecf8954f0580ca
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-08-26 20:29:05 +0900

    modified

--- IB/Library/pixhawk.py
+++ IB/Library/pixhawk.py
@@ -324,13 +324,13 @@ class Pixhawk:
             alt = "alt:" + str(alt)
             await self.lora.write(lat.encode())
             logger_info.info(lat)
-            await asyncio.sleep(0.1)
+            await asyncio.sleep(0)
             await self.lora.write(lng.encode())
             logger_info.info(lng)
-            await asyncio.sleep(0.1)
+            await asyncio.sleep(0)
             await self.lora.write(alt.encode())
             logger_info.info(alt)
-            await asyncio.sleep(0.1)
+            await asyncio.sleep(0)
             
             
     async def get_gps(self):
