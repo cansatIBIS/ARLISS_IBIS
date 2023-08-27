99e3f57d26cbcd846dc8f64968dc2bb030610b80
hookunsencho <namiharrypotter@yahoo.co.jp>
2023-08-27 18:28:04 +0900

    modified

--- IB/unit_test/library_test/recognition.py
+++ IB/unit_test/library_test/recognition.py
@@ -1,12 +1,22 @@
 import sys
+import numpy as np
 
 ibis_directory = "/home/pi/ARLISS_IBIS/IB/Library"
 sys.path.append(ibis_directory)
 
 from camera import Camera
 
+hsv_min_1 = np.array([0,145,0])
+hsv_max_1 = np.array([5,255,255])
+hsv_min_2 = np.array([150,110,0])
+hsv_max_2 = np.array([179,255,255])
+
 def run():
-    camera = Camera()
+    camera = Camera(hsv_min_1,
+                    hsv_max_1,
+                    hsv_min_2,
+                    hsv_max_2)
+                    
     camera.take_pic()
     res = camera.detect_center()
     print('percent={}, center={}'.format(res['percent'], res['center']))
