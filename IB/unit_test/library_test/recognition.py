69658fcabe8ced2881fe0c612ee8a175f796ba84
hookunsencho <namiharrypotter@yahoo.co.jp>
2023-08-27 18:05:03 +0900

    modified

---
+++ IB/unit_test/library_test/recognition.py
@@ -0,0 +1,16 @@
+import sys
+
+ibis_directory = "/home/pi/ARLISS_IBIS/IB/Library"
+sys.path.append(ibis_directory)
+
+from camera import Camera
+
+async def run():
+    camera = Camera()
+    camera.take_pic()
+    res = camera.detect_center()
+    print('percent={}, center={}'.format(res['percent'], res['center']))
+
+    distance = 1
+    x_m, y_m = camera.get_target_position(distance)
+    print(f"x={x_m},y={y_m}")
