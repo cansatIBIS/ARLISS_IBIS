9a954cc6efd346acefd01f9ee562c4f97feac683
hookunsencho <namiharrypotter@yahoo.co.jp>
2023-08-30 17:43:39 +0900

    modified

--- IB/Library/pixhawk.py
+++ IB/Library/pixhawk.py
@@ -724,10 +724,10 @@ class Pixhawk:
             break
         red_lat = lat_now+self.north_m*lat_deg_per_m
         red_lng = lng_now+self.east_m*lng_deg_per_m
-        is_red_below = False
-        if abs(self.image_res['center'][0])<0.1 and abs(self.image_res['center'][1])<0.1:
-            is_red_below = True
-        return red_lat, red_lng, abs_alt, is_red_below
+        is_red_right_below = False
+        if abs(self.image_res['center'][0])<0.1 and abs(self.image_res['center'][1])<0.2:
+            is_red_right_below = True
+        return red_lat, red_lng, abs_alt, is_red_right_below
         
 
     async def image_navigation_goto(self):
---
+++ IB/unit_test/library_test/image_navigation_arliss.py
@@ -0,0 +1,108 @@
+import sys
+
+ibis_directory = "/home/pi/ARLISS_IBIS/IB/Library"
+sys.path.append(ibis_directory)
+
+import asyncio
+import numpy as np
+from pixhawk import Pixhawk
+from logger_lib import logger_info
+from lora import Lora
+from camera import Camera
+
+# parameters---------------------
+fuse_PIN = 0
+wait_time = 0
+fuse_time = 0
+land_timelimit = 0
+land_judge_len = 30
+health_continuous_count = 3
+waypoint_lat = 35.79695235999999
+waypoint_lng = 139.8920656
+waypoint_alt = 10
+mission_speed = 5
+lora_power_pin = 4
+lora_sleep_time = 0
+use_camera = True
+hsv_min_1 = np.array([0,127,127])
+hsv_max_1 = np.array([5,255,255])
+hsv_min_2 = np.array([150,127,127])
+hsv_max_2 = np.array([180,255,255])
+#--------------------------------
+
+async def run():
+    
+    lora = Lora(
+        lora_power_pin,
+        lora_sleep_time     
+        )
+
+    pixhawk = Pixhawk(
+                 fuse_PIN,
+                 wait_time,
+                 fuse_time,
+                 land_timelimit,
+                 land_judge_len,
+                 health_continuous_count,
+                 waypoint_lat,
+                 waypoint_lng,
+                 waypoint_alt,
+                 mission_speed,
+                 lora,
+                 use_camera
+                 )
+    
+    camera = Camera(hsv_min_1,
+                    hsv_max_1,
+                    hsv_min_2,
+                    hsv_max_2)
+    
+    await pixhawk.connect()
+    await pixhawk.upload_mission()
+    await pixhawk.health_check()
+    await pixhawk.arm()
+    await pixhawk.start_mission()
+    await pixhawk.gather_main_coroutines()
+
+    # 高さをwaypoint_altぴったりに合わせる
+    goal_abs_alt = await pixhawk.get_position_alt()
+    goal_lidar_alt = await pixhawk.get_distance_alt()
+    await pixhawk.goto_location(waypoint_lat, waypoint_lng, goal_abs_alt - goal_lidar_alt + waypoint_alt)
+    await asyncio.sleep(5)
+
+    # 10mの高さで画像航法。赤が真下にあっても着陸はしない。
+    red_lat, red_lng, abs_alt, is_red_right_below= await pixhawk.calc_red_position()
+    logger_info.info(f"[go to] red_lat:{red_lat}, red_lng:{red_lng}, alt:{goal_abs_alt - goal_lidar_alt + 5}, abs_alt:{abs_alt}")
+    await pixhawk.goto_location(red_lat, red_lng, goal_abs_alt - goal_lidar_alt + 5)
+    await asyncio.sleep(5)
+
+    # 5mの高さで画像航法
+    red_lat, red_lng, abs_alt, is_red_right_below= await pixhawk.calc_red_position()
+    if is_red_right_below:
+        logger_info.info(f"success image navigation!")
+        await pixhawk.land()
+    else :
+        logger_info.info(f"[go to] red_lat:{red_lat}, red_lng:{red_lng}, alt:{goal_abs_alt - goal_lidar_alt + 3}, abs_alt:{abs_alt}")
+        await pixhawk.goto_location(red_lat, red_lng, goal_abs_alt - goal_lidar_alt + 3)
+        await asyncio.sleep(5)
+
+    # 3mの高さで画像航法
+    while True:
+        red_lat, red_lng, abs_alt, is_red_right_below= await pixhawk.calc_red_position()
+        logger_info.info(f"[go to] red_lat:{red_lat}, red_lng:{red_lng}, alt:{goal_abs_alt - goal_lidar_alt + 5}, abs_alt:{abs_alt}")
+        await pixhawk.goto_location(red_lat, red_lng, abs_alt)
+        await asyncio.sleep(5)
+        if is_red_right_below:
+            break
+    logger_info.info(f"success image navigation!")
+    await pixhawk.land()
+
+    
+
+
+    
+
+
+if __name__ == "__main__":
+
+    asyncio.run(run())
