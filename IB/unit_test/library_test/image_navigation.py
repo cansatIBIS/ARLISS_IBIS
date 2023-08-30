77f8f0acc4617cdd76e71dc1a0f88c8bb8dc7229
hookunsencho <namiharrypotter@yahoo.co.jp>
2023-08-30 12:05:45 +0900

    [modified] color range

--- IB/unit_test/library_test/image_navigation.py
+++ IB/unit_test/library_test/image_navigation.py
@@ -24,10 +24,10 @@ mission_speed = 5
 lora_power_pin = 4
 lora_sleep_time = 0
 use_camera = True
-hsv_min_1 = np.array([0,145,0])
+hsv_min_1 = np.array([0,127,127])
 hsv_max_1 = np.array([5,255,255])
-hsv_min_2 = np.array([0,110,0])
-hsv_max_2 = np.array([179,255,255])
+hsv_min_2 = np.array([150,127,127])
+hsv_max_2 = np.array([180,255,255])
 #--------------------------------
 
 async def run():
