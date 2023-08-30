ad71fda04776b6386fcce912de45a77247cf993e
Kwskayumu <kwskayumu@icloud.com>
2023-08-30 12:50:06 +0900

    [created] created set_other_param_config.py

---
+++ IB/unit_test/param_test/set_other_param_config.py
@@ -0,0 +1,31 @@
+#8/30未実験
+import datetime
+import json
+
+file_path = "/home/pi/ARLISS_IBIS/IB/config/matsudo_config/other_param_matsudo_condig.json"
+
+with open(file_path, mode="r") as f:
+    other_param = json.load(f)
+print(other_param)
+
+#yesを押し続ければ複数パラメータ変更可能
+is_continue_setting = True
+while is_continue_setting:
+    key_name = input("Enter the key of the parameter you want to change: ")
+    parameter = input("Write down the parameters after the change: ")
+    other_param[key_name] = parameter
+    
+    choice = input("Do you want to continue? Please respond with 'yes' or 'no': ")
+    if choice in ['y', 'ye', 'yes']:
+        is_continue_setting = True
+    elif choice in ['n', 'no']:
+        is_continue_setting = False
+   
+#configの変更時刻を記録     
+dt_now = datetime.datetime.now()
+other_param["set time"] = dt_now
+
+with open(file_path, mode="w") as f:
+    json.dump(other_param, f, indent=4)
+
+print("Parameters have been updated")
