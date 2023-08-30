019ff8987eb039d649c5984ed769695b390536fb
Kwskayumu <kwskayumu@icloud.com>
2023-08-30 13:31:35 +0900

    [modified]

--- IB/unit_test/param_test/set_other_param_config.py
+++ IB/unit_test/param_test/set_other_param_config.py
@@ -11,21 +11,21 @@ print(other_param)
 #yesを押し続ければ複数パラメータ変更可能
 is_continue_setting = True
 while is_continue_setting:
-    key_name = input("Enter the key of the parameter you want to change: ")
-    parameter = input("Write down the parameters after the change: ")
+    key_name = input("enter the key of the parameter you want to change: ")
+    parameter = input("write down the parameters after the change: ")
     other_param[key_name] = parameter
     
-    choice = input("Do you want to continue? Please respond with 'yes' or 'no': ")
+    choice = input("do you want to continue? Please respond with 'yes' or 'no': ")
     if choice in ['y', 'ye', 'yes']:
         is_continue_setting = True
     elif choice in ['n', 'no']:
         is_continue_setting = False
    
 #configの変更時刻を記録     
-dt_now = datetime.datetime.now()
+dt_now = datetime.datetime.now().isoformat()
 other_param["set time"] = dt_now
 
 with open(file_path, mode="w") as f:
     json.dump(other_param, f, indent=4)
 
-print("Parameters have been updated")
+print("all parameters have been updated")
