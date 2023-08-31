03ca020aa9685fe74dd541d05cea7e457434707a
hookunsencho <namiharrypotter@yahoo.co.jp>
2023-08-31 14:45:33 +0900

    modified

--- IB/unit_test/library_test/recognition.py
+++ IB/unit_test/library_test/recognition.py
@@ -6,10 +6,10 @@ sys.path.append(ibis_directory)
 
 from camera import Camera
 
-hsv_min_1 = np.array([0,145,0])
+hsv_min_1 = np.array([0, 85, 0])
 hsv_max_1 = np.array([5,255,255])
-hsv_min_2 = np.array([0,110,0])
-hsv_max_2 = np.array([179,255,255])
+hsv_min_2 = np.array([150,85,0])
+hsv_max_2 = np.array([180,255,255])
 
 def run():
     camera = Camera(hsv_min_1,
