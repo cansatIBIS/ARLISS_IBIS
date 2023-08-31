ec14e6564c3f5c9c2ec9400e599477904e987116
hookunsencho <namiharrypotter@yahoo.co.jp>
2023-08-31 21:39:29 +0900

    modified

--- IB/Library/camera.py
+++ IB/Library/camera.py
@@ -47,7 +47,7 @@ class Camera:
 
     def take_pic(self):
 
-        self.camera.exposure_mode = "off"
+        # self.camera.exposure_mode = "off"
         self.camera.exposure_compensation = -5
         time.sleep(2)
         logger_info.info("taking pic...: {}".format(self.image_path))
