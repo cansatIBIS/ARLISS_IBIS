83e7a3cfb985a11cd239f2b9ff9afaff3cc01935
hookunsencho <namiharrypotter@yahoo.co.jp>
2023-09-03 21:47:28 +0900

    modified

--- IB/unit_test/library_test/image_navigation_arliss.py
+++ IB/unit_test/library_test/image_navigation_arliss.py
@@ -92,7 +92,7 @@ async def run():
                  waypoint_alt,
                  mission_speed,
                  lora,
-                 use_camera,
+                 use_camera = True,
                  use_gps_config = False
                  )
       
