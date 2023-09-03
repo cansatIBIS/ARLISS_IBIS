5e20e0e9144bc7064253c9a0ec22c21afead26d5
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-09-03 19:39:24 +0900

    modified lora_write_test

--- IB/unit_test/library_test/lora_write_test.py
+++ IB/unit_test/library_test/lora_write_test.py
@@ -8,7 +8,7 @@ from lora import Lora
 
 # parameters---------------------
 lora_power_pin = 4
-lora_sleep_time = 10
+lora_sleep_time = 3
 #--------------------------------
 
 async def run():
@@ -21,11 +21,17 @@ async def run():
     print("Take care that raspi uses pin power suply")
     lora.serial_connect()
     print("sending")
+    print("lat:error")
     await lora.write("lat:error")
+    print("land judge start")
     await lora.write("land judge start")
     msg = "land judge start"
+    print(msg)
     await lora.write(msg)
+    print("1")
     await lora.write("1")
+    print("z")
+    await lora.write("z")
 
 
 if __name__ == "__main__":
