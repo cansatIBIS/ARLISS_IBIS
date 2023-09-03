6ce8708731ac5dfb7b1383a9d4b957a5a10f79a7
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-09-03 19:00:26 +0900

    modified lora_write_test

--- IB/unit_test/library_test/lora_write_test.py
+++ IB/unit_test/library_test/lora_write_test.py
@@ -1,24 +1,28 @@
 import sys
-import asyncio
 
 ibis_directory = "/home/pi/ARLISS_IBIS/IB/Library"
 sys.path.append(ibis_directory)
 
+import asyncio
 from lora import Lora
 
-
-lora_sleep_time = 3 
+# parameters---------------------
 lora_power_pin = 4
-
+lora_sleep_time = 4
+#--------------------------------
 
 async def run():
-  
-  lora = Lora(lora_power_pin,
-              lora_sleep_time)
-  await lora.write("land judge start")
-  
+
+    lora = Lora(
+        lora_power_pin,
+        lora_sleep_time
+    )
+    
+    print("Take care that raspi uses pin power suply")
+    lora.serial_connect()
+    lora.write("land judge start")
 
 
 if __name__ == "__main__":
-    
+
     asyncio.run(run())
