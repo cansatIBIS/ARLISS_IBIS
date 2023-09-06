a403a894be94988ec938a87679b14fa5b59f06f8
hookunsencho <namiharrypotter@yahoo.co.jp>
2023-09-06 23:58:25 +0900

    modified

--- IB/Library/pixhawk.py
+++ IB/Library/pixhawk.py
@@ -739,7 +739,7 @@ class Pixhawk:
         logger_info.info("Land start")
         self.is_in_air = True
         while True:
-            await asyncio.sleep(1)
+            await asyncio.sleep(0.01)
             logger_info.info(self.is_in_air)
             if abs(float(self.roll_deg)) > 60 or abs(float(self.pitch_deg)) > 60:
                 logger_info.info("Hit the target!")
