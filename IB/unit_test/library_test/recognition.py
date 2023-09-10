f686cf30b7174a3f3672a449f6f4267c7a37c35e
hookunsencho <namiharrypotter@yahoo.co.jp>
2023-09-10 16:39:33 -0700

    modified

--- IB/unit_test/library_test/recognition.py
+++ IB/unit_test/library_test/recognition.py
@@ -18,13 +18,13 @@ def run():
                     hsv_min_2,
                     hsv_max_2)
         
-    for _ in range(3):
-        camera.take_pic()
-        res = camera.detect_center()
-        print('percent={}, center={}'.format(res['percent'], res['center']))
 
-        distance = 1
-        x_m, y_m = camera.get_target_position(distance)
-        print(f"x={x_m},y={y_m}")
+    camera.take_pic()
+    res = camera.detect_center()
+    print('percent={}, center={}'.format(res['percent'], res['center']))
+
+    # distance = 1
+    # x_m, y_m = camera.get_target_position(distance)
+    # print(f"x={x_m},y={y_m}")
 
 run()
