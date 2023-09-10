486bfc0a7d9fcabfa300c215645341a623bbb1f6
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-09-09 17:48:39 -0700

    modified ARLISS

--- IB/Performance/ARLISS.py
+++ IB/Performance/ARLISS.py
@@ -27,7 +27,7 @@ released_judge_time = 15
 lora_power_pin = 4
 deamon_pass = "/home/pi/ARLISS_IBIS/IB/log/Performance_log.txt"
 is_destruct_deamon = False
-use_camera = True
+use_camera = False
 use_gps_config = False
 use_other_param_config = False
 
