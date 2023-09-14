236acb83b33b13c7a0cce155b5b89ce084aaee5d
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-09-14 05:07:53 -0700

    modified destruct_deamon

--- IB/unit_test/library_test/destruct_deamon.py
+++ IB/unit_test/library_test/destruct_deamon.py
@@ -34,7 +34,6 @@ async def run():
   ibis = Ibis(# pixhawk
               fuse_pin,
               wait_time,
-              lora_sleep_time,
               fuse_time,
               land_timelimit,
               land_judge_len,
